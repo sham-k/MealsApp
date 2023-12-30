39f7f59e19a58b4f031e2253270c0b51192fb20b
SHAM <shamarknibbs@gmail.com>
2023-12-30 16:53:41 -0500

    Structuring the menu

---
+++ assets/components/CategoryGridTile.js
@@ -0,0 +1,25 @@
+import{Pressable,View,Text,StyleSheet} from 'react-native';
+
+function CategoryGridTile({title, color}){
+    return( 
+    <View style={styles.gridItem}>
+        <Pressable>
+            <View>
+            <Text>{title}</Text>
+            </View>
+        </Pressable>
+    </View>
+    );
+}
+
+export default CategoryGridTile;
+
+const styles =StyleSheet.create({
+    gridItem:{
+        flex: 1,
+        margin:16,
+        height: 150,
+        borderRadius:8,
+        elevation:4
+    }
+})
