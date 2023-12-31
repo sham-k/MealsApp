1f3faad899e3dd5350aa0b9fa99fffaa538dd97f
SHAM <shamarknibbs@gmail.com>
2023-12-31 18:17:42 -0500

    Added navigation for MealsOverview Screen

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 
 import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import CategoriesScreen from './assets/screens/CategoriesScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import MealsOverviewScreen from './assets/screens/MealsOverviewScreen';
+
+const Stack= createNativeStackNavigator();
 
 export default function App() {
   return (
-    <CategoriesScreen />
+    <>
+    <StatusBar style='dark'/>
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+    <Stack.Screen  name="MealsOverview" component={MealsOverviewScreen}/>
+    </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
--- assets/components/CategoryGridTile.js
+++ assets/components/CategoryGridTile.js
@@ -1,10 +1,11 @@
-import{Pressable,View,Text,StyleSheet} from 'react-native';
+import{Pressable,View,Text,StyleSheet,Platform} from 'react-native';
 
-function CategoryGridTile({title, color}){
+function CategoryGridTile({title, color,onPress}){
     return( 
-    <View style={styles.gridItem}>
-        <Pressable style={styles.button}>
-            <View style={styles.innerContainer}>
+    <View style={[styles.gridItem]}>
+        <Pressable android_ripple={{color:'#ccc'}} style={({pressed})=>[styles.button, pressed ? styles.buttonPressed:null]}
+        onPress={onPress}>
+            <View style={[styles.innerContainer, { backgroundColor:color}]}>
             <Text style={styles.title}>{title}</Text>
             </View>
         </Pressable>
@@ -26,15 +27,22 @@ const styles =StyleSheet.create({
         shadowOpacity:0.25,
         shadowOffset:{width:0, height:2},
         shadowRadius:8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
     },
     button:{
         flex:1
+    },
+    buttonPressed:{
+        opacity:0.5
+
     },
     innerContainer:{
         flex:1,
         padding:16,
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+        borderRadius: 8,
+
     },
     title:{
         fontWeight:'bold',
